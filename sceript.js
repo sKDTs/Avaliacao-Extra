074d8bacf7fa280b3c79f113411c7b01e2b96a37
KDT <kimdetarso@gmail.com>
2026-05-08 18:55:39 -0300

    Update sceript.js

--- sceript.js
+++ sceript.js
@@ -1,85 +1,96 @@
 const form = document.getElementById("contactForm");
 
-form.addEventListener("submit", function (event) {
+const status = document.getElementById("mensagemStatus");
+
+form.addEventListener("submit", function(event) {
+
   event.preventDefault();
 
-  validateForm();
-});
+  const name =
+    document.getElementById("name").value.trim();
 
-function validateForm() {
-  const name = document.getElementById("name").value.trim();
+  const email =
+    document.getElementById("email").value.trim();
 
-  const email = document.getElementById("email").value.trim();
-
-  const message = document.getElementById("message").value.trim();
-
-  const status = document.getElementById("mensagemStatus");
+  const message =
+    document.getElementById("message").value.trim();
 
   // NOME
   if (name.length < 3) {
+
     alert("O nome deve ter no mínimo 3 caracteres");
 
-    status.innerText = "O nome deve ter no mínimo 3 caracteres.";
+    status.innerText =
+      "O nome deve ter no mínimo 3 caracteres.";
 
     status.style.color = "red";
 
-    return false;
+    return;
   }
 
   // EMAIL
   if (email.length < 5) {
+
     alert("O email deve ter no mínimo 5 caracteres");
 
-    status.innerText = "O email deve ter no mínimo 5 caracteres.";
+    status.innerText =
+      "O email deve ter no mínimo 5 caracteres.";
 
     status.style.color = "red";
 
-    return false;
+    return;
   }
 
-  // REGEX EMAIL
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  // EMAIL VÁLIDO
+  const emailPattern =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!emailPattern.test(email)) {
+
     alert("Digite um email válido");
 
-    status.innerText = "Digite um email válido.";
+    status.innerText =
+      "Digite um email válido.";
 
     status.style.color = "red";
 
-    return false;
+    return;
   }
 
   // MENSAGEM MÍNIMA
   if (message.length < 3) {
+
     alert("A mensagem deve ter no mínimo 3 caracteres");
 
-    status.innerText = "A mensagem deve ter no mínimo 3 caracteres.";
+    status.innerText =
+      "A mensagem deve ter no mínimo 3 caracteres.";
 
     status.style.color = "red";
 
-    return false;
+    return;
   }
 
   // MENSAGEM MÁXIMA
   if (message.length > 265) {
+
     alert("A mensagem deve ter no máximo 265 caracteres");
 
-    status.innerText = "A mensagem deve ter no máximo 265 caracteres.";
+    status.innerText =
+      "A mensagem deve ter no máximo 265 caracteres.";
 
     status.style.color = "red";
 
-    return false;
+    return;
   }
 
   // SUCESSO
   alert("Formulário enviado com sucesso!");
 
-  status.innerText = "Mensagem enviada com sucesso!";
+  status.innerText =
+    "Mensagem enviada com sucesso!";
 
   status.style.color = "green";
 
   form.reset();
 
-  return true;
-}
+});
